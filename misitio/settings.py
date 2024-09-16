d852394d1b3da31d32faea2b520fb89c46c792b7
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-09-16 13:01:44 +0200

    canvi de títol a des template de admin, millores a admin

--- misitio/settings.py
+++ misitio/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
     'polls.apps.PollsConfig',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'misitio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
